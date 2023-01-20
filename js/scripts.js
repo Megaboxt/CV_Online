1cde8ee206a6085077651919b137b7c53464fbc4
Gonzalo Duarte <megaboxtpro@gmail.com>
2023-01-20 01:44:38 -0300

    Update header

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,31 @@ addEventListener('DOMContentLoaded', ()=> {
     window.addEventListener('scroll', indicarScroll)
 
 });
+
+/* Menu hamburguesa */
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars() {
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
+
+/* Activar menu hambuerguesa */
+document.getElementById("btn-menu").addEventListener("click", mostrar_menu);
+document.getElementById("back-menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+back_menu = document.getElementById("back-menu");
+
+function mostrar_menu() {
+    
+}
+
+function ocultar_menu() {
+    back_menu.style.display = "none";
+}
